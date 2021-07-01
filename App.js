4ba0bd879c61dd1e94ab75d4b35e25a2a9633f6b
ryankoch13 <ryan.koch13@gmail.com>
2021-06-30 20:30:10 -0700

    added back bone for authentication

--- App.js
+++ App.js
@@ -1,21 +1,92 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { Component } from 'react'
+import firebase from 'firebase'
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyAa1IhljkE5BGzLhx_AWLIb4uRvPDhE9tE",
+  authDomain: "ryansale-b0b97.firebaseapp.com",
+  projectId: "ryansale-b0b97",
+  storageBucket: "ryansale-b0b97.appspot.com",
+  messagingSenderId: "935213998931",
+  appId: "1:935213998931:web:55d360869f98213a2937a6",
+  measurementId: "G-4HQD5K086K"
+};
+
+if(firebase.apps.length === 0){
+  firebase.initializeApp(firebaseConfig)
+}
+
+import { StyleSheet, Text, View, Image, SafeAreaView, TouchableOpacity } from 'react-native'
+
+import { NavigationContainer } from '@react-navigation/native'
+import { createStackNavigator } from '@react-navigation/stack'
+
+import LandingScreen from './components/auth/Landing'
+import RegisterScreen from './components/auth/Register'
+
+const Stack = createStackNavigator()
+
+export class App extends Component {
+  constructor(props){
+    super(props)
+
+    this.state = {
+      loaded: false,
+    }
+  }
+
+  componentDidMount(){
+    firebase.auth().onAuthStateChanged((user) => {
+      if(!user){
+        this.setState({
+          loggedIn: false,
+          loaded: true
+        })
+      } else {
+        this.setState({
+          loggedIn: true,
+          loaded: true,
+        })
+      }
+    })
+  }
+
+  render() {
+    const { loggedIn, loaded } = this.state
+    if(!loaded){
+      return(
+        <View style={ styles.container }>
+          <Text>Loading...</Text>
+        </View>
+
+      )
+    }
+    
+    if(!loggedIn){
+      return (
+          <NavigationContainer>
+        <Stack.Navigator initialRouteName="Landing">
+          <Stack.Screen name="Landing" component={ LandingScreen } options={{ headerShown: false }}/>
+          <Stack.Screen name="Register" component={ RegisterScreen } />
+        </Stack.Navigator>
+      </NavigationContainer>
+      )
+    }
+
+    return (
+      <View style={ styles.container }>
+          <Text>User is logged in.</Text>
+        </View>
+    )
+  }
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
     justifyContent: 'center',
-  },
-});
+    alignItems: 'center'
+  }
+})
+
+export default App
